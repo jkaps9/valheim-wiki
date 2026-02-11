923e701073fe840c958b059024b49177cda7cc1f
jkaps9 <15219491+jkaps9@users.noreply.github.com>
2026-02-10 22:09:20 -0500

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,61 @@
+import path from "node:path";
+import * as sass from "sass";
+import { DateTime } from "luxon";
+
+export default function (config) {
+  // add SCSS template format
+  config.addTemplateFormats("scss");
+
+  config.addPassthroughCopy("./src/main.js");
+
+  // Set directories to pass through to the dist folder
+  config.addPassthroughCopy("./src/assets");
+  config.addPassthroughCopy("./src/scripts");
+  config.addPassthroughCopy("./src/admin");
+
+  // Configure SCSS files
+  config.addExtension("scss", {
+    outputFileExtension: "css",
+
+    // opt-out of Eleventy Layouts
+    useLayouts: false,
+
+    compile: async function (inputContent, inputPath) {
+      let parsed = path.parse(inputPath);
+      // Don’t compile file names that start with an underscore
+      if (parsed.name.startsWith("_")) {
+        return;
+      }
+
+      let result = sass.compileString(inputContent, {
+        loadPaths: [parsed.dir || ".", this.config.dir.includes],
+      });
+
+      // Map dependencies for incremental builds
+      this.addDependencies(inputPath, result.loadedUrls);
+
+      return async (data) => {
+        return result.css;
+      };
+    },
+  });
+
+  config.addWatchTarget("./src/scss/");
+
+  // add date filter
+  config.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_FULL);
+  });
+
+  return {
+    pathPrefix:
+      process.env.NODE_ENV === "production" ? "/eleventy-template/" : "/",
+    markdownTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dir: {
+      input: "src",
+      output: "dist",
+    },
+  };
+}
